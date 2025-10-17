c779f7eccbe3ef5ac56932d1af9833f3e7d99258
JhoanPragma <jhoan.gonzaleze@pragma.com.co>
2025-10-16 23:56:59 -0500

    zipp

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,9 @@
-output "api_endpoint" {
-  description = "URL base del API Gateway HTTP para la invocación de servicios."
-  value       = aws_apigatewayv2_api.torneos_api.api_endpoint
-}
+# output "api_endpoint" {
+#   description = "Endpoint de la API"
+#   value       = aws_apigatewayv2_api.torneos_api.api_endpoint
+# }
 
-output "sns_topic_arn" {
-  description = "ARN del tópico de SNS para la configuración de notificaciones."
-  value       = aws_sns_topic.notifications_topic.arn
-}
-
-output "s3_bucket_name" {
-  description = "Nombre del Bucket S3 para el almacenamiento de códigos QR."
-  value       = aws_s3_bucket.qr_bucket.id
-}
+# output "sns_topic_arn" {
+#   description = "ARN del SNS Topic de notificaciones"
+#   value       = aws_sns_topic.notifications_topic.arn
+# }
